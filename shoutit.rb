553097cc6f11dec13314d97b217fe1b5586ec650
Dayne Broderson <dayne@gina.alaska.edu>
2008-05-21 13:07:37 -0800

    working shoutr

--- shoutit.rb
+++ shoutit.rb
@@ -5,29 +5,43 @@
 
 require 'rubygems'
 require 'shout'
+require 'yaml'
 
 BLOCKSIZE = 16384
 
+c = YAML.load_file('config.yml')
 s = Shout.new
-s.host = "localhost"
-s.port = 8008
-s.mount = "/shoutr.ogg"
-s.user = "source"
+s.host     = c[:host]  || "localhost" # beef.gina.alaska.edu
+s.port     = c[:port]  || "8000"      # 8008
+s.mount    = c[:mount] || "/shoutr"   #
+s.user     = c[:user]  || "source"    # 
+s.password = c[:password] || "hackme" # 
 
 s.connect
 
-ARGV.each do |filename|
-	File.open(filename) do |file|
-		puts "sending data from #{filename}"
-		m = ShoutMetadata.new
-		m.add 'filename', filename
-		s.metadata = m
+def send_io(s,p,filename)
+  puts "sending data from #{filename}"
+  m = ShoutMetadata.new
+  m.add 'filename', File.basename(filename)
+  s.metadata = m
 
-		while data = file.read(BLOCKSIZE)
-			s.send data
-			s.sync
-		end
-	end
+  while data = p.read(BLOCKSIZE)
+    s.send data
+    s.sync
+  end
+end
+
+ARGV.each do |filename|
+  target = filename if File.exists?(filename)
+  next unless target
+  if File.extname(target).downcase == '.mp3'
+    puts "got an mp3 file, trying new code"
+    cmd = ("mpg123 -q -w - '#{target}' | oggenc -c '#{target}' -Q -")
+    IO.popen(cmd,'r') { |p| send_io(s,p,filename) } 
+  elsif File.extname(target).downcase == '.ogg'
+    puts "got an ogg file, using old code" 
+	  File.open(filename) { |f| send_io(s,f,filename) }
+  end
 end
 
 s.disconnect
