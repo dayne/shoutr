146d02fcacbb145ae82a51af5f710c35ad4b3d60
Dayne Broderson <dayne@gina.alaska.edu>
2008-05-20 22:05:32 -0800

    initial shoutit.rb

---
+++ shoutit.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+# Stream all the files given on the commandline to the Icecast server on
+# localhost. 
+
+require 'rubygems'
+require 'shout'
+
+BLOCKSIZE = 16384
+
+s = Shout.new
+s.host = "localhost"
+s.port = 8008
+s.mount = "/shoutr.ogg"
+s.user = "source"
+
+s.connect
+
+ARGV.each do |filename|
+	File.open(filename) do |file|
+		puts "sending data from #{filename}"
+		m = ShoutMetadata.new
+		m.add 'filename', filename
+		s.metadata = m
+
+		while data = file.read(BLOCKSIZE)
+			s.send data
+			s.sync
+		end
+	end
+end
+
+s.disconnect
